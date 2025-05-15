743037df604f358dfb0eef435a6f1d641c8b2b97
augusto.savi <augusto.savi@betha.com.br>
2025-05-15 09:36:34 -0300

    feat: enhance InscricaoMatricula component with breadcrumb navigation and active registration flow

--- src/app/pages/components/inscricao-matricula/inscricao-matricula.component.ts
+++ src/app/pages/components/inscricao-matricula/inscricao-matricula.component.ts
@@ -2,19 +2,46 @@ import { Component } from '@angular/core';
 import { StepComponent } from '../step/step.component';
 import { CommonModule } from '@angular/common';
 import { AppInscricaoListaEsperaNomeComponent } from '../app-inscricao-lista-espera-nome/app-inscricao-lista-espera-nome.component';
+import { BrBreadcrumb, BrCollapse, BrDivider, BrIcon, BrItem, BrList, BrMessage } from '@govbr-ds/webcomponents-angular/standalone';
+
+interface Crumb {
+  label: string;
+  url?: string;
+  target?: string;
+  active?: boolean;
+}
 
 @Component({
   selector: 'app-inscricao-matricula',
   standalone: true,
-  imports: [CommonModule, StepComponent, AppInscricaoListaEsperaNomeComponent],
+  imports: [CommonModule, StepComponent, AppInscricaoListaEsperaNomeComponent, BrMessage, BrBreadcrumb, BrDivider, BrList, BrCollapse, BrItem, BrIcon],
   templateUrl: './inscricao-matricula.component.html',
   styleUrl: './inscricao-matricula.component.scss'
 })
 export class InscricaoMatriculaComponent {
+  isInscricaoAtiva = false;
+
+  /** Lista de páginas ancestrais + página atual */
+  links: Crumb[] = [
+    { label: 'Explorar serviços', url: '/', target: '_self' },
+    { label: 'Inscrição lista de espera 2025', active: true },
+  ];
+
+    
   steps = ['Nome', 'Nível/Modalidade', 'Formulário', 'Confirmação', 'Comprovante'];
   selectedStep = 0;
 
   onStepChange(index: number) {
     this.selectedStep = index;
+  }
+
+  nextStep() {
+    if (this.selectedStep < this.steps.length - 1) {
+      this.selectedStep++;
+    }
+  }
+
+  iniciarInscricao(): void  {
+    this.isInscricaoAtiva = true;
   }
 }
